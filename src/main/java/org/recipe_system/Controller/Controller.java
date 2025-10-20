bb86ef2726caacf6e48ec9888c536a4011503fd1
Lucas Hungaro <lhungaro10@gmail.com>
2025-10-20 18:17:10 -0300

    fix: modify functions signs and validations

--- src/main/java/org/recipe_system/Controller/Controller.java
+++ src/main/java/org/recipe_system/Controller/Controller.java
@@ -3,7 +3,6 @@ package org.recipe_system.Controller;
 import org.recipe_system.Catalog.IngredientCatalog;
 import org.recipe_system.Model.Ingredient;
 import org.recipe_system.Model.User;
-import org.recipe_system.Model.System;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -23,13 +22,13 @@ public class Controller {
 
 
     public Boolean registerIngredient(String name, int qtd_in_stock) {
-        if(this.validateIngredientNameIsEmpty(name)){
+        if(this.validateName(name)){
             return false;
         }
         List<Ingredient> ingredients = this.ingredientCatalog.getIngredients();
         Ingredient ingredient = new Ingredient(name, qtd_in_stock);
 
-        Integer numberInStock = this.validateNumberInStock(ingredient);
+        Integer numberInStock = this.getNumberInStock(ingredient);
 
         if (numberInStock > 0) {
             ingredient.setQtd_in_stock(numberInStock + qtd_in_stock);
@@ -39,12 +38,13 @@ public class Controller {
     }
 
     public Boolean editIngredient(Ingredient ingredient, String name, int qtd_in_stock) {
-        if(this.validateIngredientNameIsEmpty(name)){
+        if(!this.validateName(name) || !this.validateNumber(qtd_in_stock)){
             return false;
         }
+
         List<Ingredient> ingredients = this.ingredientCatalog.getIngredients();
 
-        Integer numberInStock = this.validateNumberInStock(new Ingredient(name, qtd_in_stock));
+        Integer numberInStock = this.getNumberInStock(new Ingredient(name, qtd_in_stock));
 
         if (numberInStock == 0) {
             return false;
@@ -57,12 +57,17 @@ public class Controller {
         return this.ingredientCatalog.getIngredients();
     }
 
-    private Integer validateNumberInStock(Ingredient ingredient) {
+    private Integer getNumberInStock(Ingredient ingredient) {
         return this.ingredientCatalog.getQtdInStock(ingredient);
     }
 
-    private Boolean validateIngredientNameIsEmpty(String name){
-        return name.isEmpty();
+    private Boolean validateNumber(Integer qtd_in_stock) {
+        return qtd_in_stock >= 0;
+    }
+
+    // o retorno foi negado aqui para o nome da função fazer sentido.
+    private Boolean validateName(String name){
+        return !name.isEmpty();
     }
 
 }
